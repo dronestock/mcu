0837eac8b1e06f88751ce1e49828452b78408379
storezhang <storezhang@gmail.com>
2021-10-12 16:59:26 +0800

    项目初始化

--- config.go
+++ config.go
@@ -10,6 +10,9 @@ type config struct {
 func (c *config) dependencyStrings() (strings []string) {
 	strings = make([]string, 0, len(c.dependencies))
 	for _, _dependency := range c.dependencies {
+		if nil == _dependency {
+			continue
+		}
 		strings = append(strings, _dependency.String())
 	}
 
