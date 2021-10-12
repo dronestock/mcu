138ee5bb68f429e34b2478b3ca9cdb97d32ffa22
storezhang <storezhang@gmail.com>
2021-10-12 18:48:59 +0800

    项目初始化

--- js.go
+++ js.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	`fmt`
 	`path`
 
 	`github.com/storezhang/glog`
@@ -14,10 +15,18 @@ func js(conf *config, _ glog.Logger) (err error) {
 		conf.path = path.Join(conf.path, "package.json")
 	}
 
-	err = json(conf.path, jsonElement{
+	dependencies := make([]jsonElement, 0, len(conf.dependencies)+1)
+	dependencies = append(dependencies, jsonElement{
 		path:  "version",
 		value: conf.version,
 	})
+	for _, _dependency := range conf.dependencies {
+		dependencies = append(dependencies, jsonElement{
+			path:  fmt.Sprintf("dependencies.%s", _dependency.module),
+			value: _dependency.version,
+		})
+	}
+	err = json(conf.path, dependencies...)
 
 	return
 }
