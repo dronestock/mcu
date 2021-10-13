d2fb88af929be26fb3dd65e5d790fc73e861a249
storezhang <storezhang@gmail.com>
2021-10-13 23:10:07 +0800

    修复Dart写入

--- dart.go
+++ dart.go
@@ -14,26 +14,25 @@ func dart(conf *config, _ glog.Logger) (err error) {
 	if dir, dirErr := gox.IsDir(conf.path); nil != dirErr {
 		panic(dirErr)
 	} else if dir {
-		conf.path = path.Join(conf.path, "pubspec.yaml")
+		conf.path = path.Join(conf.path, `pubspec.yaml`)
 	}
 
-	commands := []string{"eval"}
+	commands := []string{`eval`}
 	environments := make([]string, 0, len(conf.dependencies))
-
 	updates := make([]string, 0, len(conf.dependencies))
-	environments = append(environments, fmt.Sprintf("%s=%s", "version", conf.version))
-	updates = append(updates, ".version=strenv(version)")
+	environments = append(environments, fmt.Sprintf(`%s=%s`, "version", conf.version))
+	updates = append(updates, `.version=strenv(version)`)
 	for _, _dependency := range conf.dependencies {
 		// 使用随机字符串是为了防止原始字符串里面出现环境变量不允许的字符
 		version := gox.RandString(16)
-		updates = append(updates, fmt.Sprintf(".dependencies.%s = strenv(%s)", _dependency.module, version))
+		updates = append(updates, fmt.Sprintf(`.dependencies.%s = strenv(%s)`, _dependency.module, version))
 		environments = append(
 			environments,
-			fmt.Sprintf("%s=%s", version, _dependency.version),
+			fmt.Sprintf(`%s=%s`, version, _dependency.version),
 		)
 	}
-	commands = append(commands, strings.Join(updates, " | "), conf.path)
-	commands = append(commands, "--inplace", "--prettyPrint")
+	commands = append(commands, strings.Join(updates, ` | `), conf.path)
+	commands = append(commands, `--inplace`, `--prettyPrint`)
 
 	// 执行命令
 	cmd := exec.Command(`yq`, commands...)
