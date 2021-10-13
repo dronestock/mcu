58cbd617000888fc19b91396d6746b984af895d9
storezhang <storezhang@gmail.com>
2021-10-13 23:08:46 +0800

    修复Dart写入

--- dart.go
+++ dart.go
@@ -17,30 +17,26 @@ func dart(conf *config, _ glog.Logger) (err error) {
 		conf.path = path.Join(conf.path, "pubspec.yaml")
 	}
 
-	commands := []string{
-		"e",
-		"--inplace",
-	}
+	commands := []string{"eval"}
 	environments := make([]string, 0, len(conf.dependencies))
 
 	updates := make([]string, 0, len(conf.dependencies))
 	environments = append(environments, fmt.Sprintf("%s=%s", "version", conf.version))
 	updates = append(updates, ".version=strenv(version)")
 	for _, _dependency := range conf.dependencies {
-		module := gox.RandString(16)
+		// 使用随机字符串是为了防止原始字符串里面出现环境变量不允许的字符
 		version := gox.RandString(16)
-		updates = append(updates, fmt.Sprintf(".dependencies.strenv(%s)=strenv(%s)", module, version))
+		updates = append(updates, fmt.Sprintf(".dependencies.%s = strenv(%s)", _dependency.module, version))
 		environments = append(
 			environments,
-			fmt.Sprintf("%s=%s", module, _dependency.module),
 			fmt.Sprintf("%s=%s", version, _dependency.version),
 		)
 	}
 	commands = append(commands, strings.Join(updates, " | "), conf.path)
-	commands = append(commands, "--prettyPrint")
+	commands = append(commands, "--inplace", "--prettyPrint")
 
 	// 执行命令
-	cmd := exec.Command(`D:\Apps\Yq\yq.exe`, commands...)
+	cmd := exec.Command(`yq`, commands...)
 	cmd.Env = append(cmd.Env, environments...)
 	err = cmd.Wait()
 
