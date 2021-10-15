a06615c52aba312f8caf77a45c4d99f31f233341
Usmile <blackmy@foxmail.com>
2021-10-15 14:12:21 +0800

    Dart配置更新

--- dart.go
+++ dart.go
@@ -1,15 +1,15 @@
 package main
 
 import (
-	`fmt`
-	`os`
-	`os/exec`
-	`path`
-	`strings`
+	"fmt"
+	"os"
+	"os/exec"
+	"path"
+	"strings"
 
-	`github.com/storezhang/glog`
-	`github.com/storezhang/gox`
-	`github.com/storezhang/gox/field`
+	"github.com/storezhang/glog"
+	"github.com/storezhang/gox"
+	"github.com/storezhang/gox/field"
 )
 
 func dart(conf *config, logger glog.Logger) (err error) {
@@ -24,10 +24,16 @@ func dart(conf *config, logger glog.Logger) (err error) {
 	updates := make([]string, 0, len(conf.dependencies))
 	environments = append(environments, fmt.Sprintf(`%s=%s`, "version", conf.version))
 	updates = append(updates, `.version=strenv(version)`)
+	replacesMap := toReplacesMap(conf.replaces)
 	for _, _dependency := range conf.dependencies {
 		// 使用随机字符串是为了防止原始字符串里面出现环境变量不允许的字符
 		version := gox.RandString(16)
-		updates = append(updates, fmt.Sprintf(`.dependencies."%s" = strenv(%s)`, _dependency.name, version))
+		if _replace, ok := replacesMap[_dependency.name]; ok == true {
+			updates = append(updates, fmt.Sprintf(`.dependencies."%s.git.url" = strenv(%s)`, _dependency.name, _replace.to.name))
+			updates = append(updates, fmt.Sprintf(`.dependencies."%s.git.ref = strenv(%s)`, _dependency.name, _replace.to.version))
+		} else {
+			updates = append(updates, fmt.Sprintf(`.dependencies."%s" = strenv(%s)`, _dependency.name, version))
+		}
 		environments = append(
 			environments,
 			fmt.Sprintf(`%s=%s`, version, _dependency.version),
@@ -45,5 +51,13 @@ func dart(conf *config, logger glog.Logger) (err error) {
 		logger.Warn(`修改Dart模块描述文件出错`, field.String(`output`, string(output)), field.Error(err))
 	}
 
+	return
+}
+
+func toReplacesMap(replaces []*replace) (replacesMap map[string]*replace) {
+	replacesMap = make(map[string]*replace)
+	for _, _replace := range replaces {
+		replacesMap[_replace.from.name] = _replace
+	}
 	return
 }
